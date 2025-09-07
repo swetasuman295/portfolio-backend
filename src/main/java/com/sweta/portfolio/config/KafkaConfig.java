fbedc31f6116d8eb6d9aff7c40100fa1bdd699a6
sweta suman <swetasuman295@gmail.com>
2025-09-07 23:46:01 +0200

    added application-ci.properties to use h2 config in git and ignores kafka in git

--- src/main/java/com/sweta/portfolio/config/KafkaConfig.java
+++ src/main/java/com/sweta/portfolio/config/KafkaConfig.java
@@ -4,11 +4,13 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
 
 @Configuration
+@Profile("!ci") // 
 public class KafkaConfig {
     
     
